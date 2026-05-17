90e97fd6efeef7b42034294ee8b55ff0601b8eaf
Syriuu <104240557@student.vgu.edu.vn>
2026-05-17 22:48:11 +0700

    modify files

--- nfa_visualizer/src/js/index.js
+++ nfa_visualizer/src/js/index.js
@@ -2,20 +2,11 @@ import DraggableCanvas from './canvas/draggable_canvas.js'
 import VisualFSA from './fsa/visual_fsa.js'
 import * as utils from './util/util.js'
 import FSADescription from './elements/fsa_description.js'
-import NFAConverter from './fsa/nfa_converter.js'
-import AnimatedNFAConverter from './fsa/animated_nfa_converter.js'
 
 const nfa = {
     visual: new VisualFSA(new DraggableCanvas('#nfa'), false),
     desc: new FSADescription('#nfa-delta-transitions')
 }
-
-const dfa = {
-    visual: new VisualFSA(new DraggableCanvas('#dfa'), true),
-    desc: new FSADescription('#dfa-delta-transitions')
-}
-
-let converter
 
 nfa.visual.addEventListener('change', () => {
     if (nfa.visual.fsa.states.length > 0) {
@@ -97,7 +88,6 @@ nfa.visual.addEventListener('change', updateTestStringResult)
 draw()
 function draw () {
     nfa.visual.draggableCanvas.draw()
-    dfa.visual.draggableCanvas.draw()
     window.requestAnimationFrame(draw)
 }
 
@@ -108,7 +98,6 @@ function draw () {
  */
 function setEditButtonsState (enabled) {
     document.querySelector('#nfa-reset').disabled = !enabled
-    document.querySelector('#nfa-convert').disabled = !enabled
 }
 
 
@@ -118,95 +107,9 @@ function setEditButtonsState (enabled) {
  */
 document.querySelector('#nfa-reset').addEventListener('click', () => {
     nfa.visual.reset()
-    document.querySelector('#dfa-column').style.display = 'none'
-    if (converter) converter.stop()
-    converter = undefined
-    document.querySelector('#nfa-convert').disabled = false
-    document.querySelector('#dfa-play').disabled = false
-    document.querySelector('#dfa-step-forward').disabled = false
-    document.querySelector('#dfa-step-backward').disabled = false
-    document.querySelector('#dfa-complete').disabled = false
 })
 
-document.querySelector('#nfa-convert').addEventListener('click', () => {
-    try {
-        if (!nfa.visual.fsa.startState) {
-            throw new Error('Please select a start state first.')
-        }
 
-        const nfaConverter = new NFAConverter(nfa.visual.fsa)
-        converter = new AnimatedNFAConverter(nfaConverter, dfa.visual, 500)
-
-        // Show the DFA column
-        document.querySelector('#dfa-column').style.display = 'block'
-        document.querySelector('#nfa-convert').disabled = true
-
-        converter.addEventListener('stop', () => {
-            document.querySelector('#dfa-play-text').innerText = 'Play'
-            document.querySelector('#dfa-play-icon').classList.replace('mdi-pause', 'mdi-play')
-        })
-
-        converter.addEventListener('start', () => {
-            document.querySelector('#dfa-play-text').innerText = 'Pause'
-            document.querySelector('#dfa-play-icon').classList.replace('mdi-play', 'mdi-pause')
-        })
-
-        converter.addEventListener('complete', () => {
-            document.querySelector('#dfa-play').disabled = true
-            document.querySelector('#dfa-step-forward').disabled = true
-            document.querySelector('#dfa-step-backward').disabled = true
-            document.querySelector('#dfa-complete').disabled = true
-        })
-
-        dfa.visual.addEventListener('change', () => {
-            dfa.desc.update(dfa.visual.fsa, false)
-        })
-
-        // Step forward once initially
-        converter.step(utils.showWarning)
-    } catch (e) {
-        utils.showWarning(e.message)
-    }
-})
-
-document.querySelector('#dfa-play').addEventListener('click', () => {
-    if (!converter) return
-    if (converter.interval) {
-        converter.stop()
-    } else {
-        converter.play(utils.showWarning)
-    }
-})
-
-document.querySelector('#dfa-step-forward').addEventListener('click', () => {
-    if (converter) {
-        converter.stop()
-        converter.step(utils.showWarning)
-    }
-})
-
-document.querySelector('#dfa-step-backward').addEventListener('click', () => {
-    if (converter) {
-        converter.stop()
-        const [prevDFA, prevStep] = converter.converter.stepBackward()
-        if (prevDFA && prevStep) {
-            converter.visualDFA.undoStep(prevStep, prevDFA)
-            document.querySelector('#dfa-conversion-step').innerHTML = 'Undo previous step'
-        }
-    }
-})
-
-document.querySelector('#dfa-complete').addEventListener('click', () => {
-    if (converter) {
-        converter.stop()
-        const steps = converter.converter.complete()
-        for (const [newDFA, step] of steps) {
-            converter.visualDFA.performStep(step, newDFA)
-        }
-        document.querySelector('#dfa-conversion-step').innerHTML = 'Conversion complete'
-        converter.dispatchEvent('complete')
-    }
-})
 
 
 
